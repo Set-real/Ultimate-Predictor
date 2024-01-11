390df1eff266d4b98f790173d5e893081fb13e80
Максим Груздев <maestrosuper@mail.ru>
2024-01-11 19:28:38 +0300

    Add predictionConfig

--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,7 @@ namespace Ultimate_Predictor
 {
     public partial class Form1 : Form
     {
+        private const string AppName = "Предсказатель";
         public Form1()
         {
             InitializeComponent();
@@ -20,19 +21,51 @@ namespace Ultimate_Predictor
 
         private async void bPredict_Click(object sender, EventArgs e)
         {
+            bPredict.Enabled = false;
             await Task.Run(() =>
             {
-                this.Invoke(new Action(() =>
+
+                for (int i = 1; i <= 100; i++)
                 {
-                    for (int i = 1; i <= 100; i++)
+                    this.Invoke(new Action(() =>
                     {
-                        progressBar1.Value = i;
-                        Thread.Sleep(200);
-                    }
-                }));
+                        UpdateProgressBar(i);
+                        this.Text = $"{i}%";
+                    }));
+                    
+                    Thread.Sleep(37); // Имитирую бурную работу программы и сложную логику
+                }
             });
 
             MessageBox.Show("Предсказание");
+
+            progressBar1.Value = 0;
+            this.Text = AppName;
+
+            bPredict.Enabled = true;
+        }
+        /// <summary>
+        /// Метод для лечения бага заполняющей строки, как работает не понимаю
+        /// </summary>
+        /// <param name="intValue"></param>
+        private void UpdateProgressBar(int intValue)
+        {
+            if (intValue == progressBar1.Maximum)
+            {
+                progressBar1.Maximum = intValue + 1;
+                progressBar1.Value = intValue + 1;
+                progressBar1.Maximum = intValue;
+            }
+            else
+            {
+                progressBar1.Value = intValue + 1;
+            }
+            progressBar1.Value = intValue;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = AppName;
         }
     }
 }
